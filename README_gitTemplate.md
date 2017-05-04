85b3cdd1e992635b662fb9852ac257a60b6e267b
ellingworthl <ellingworthl@hotmail.com>
2017-05-04 18:36:38 +0100

    add proj link to client_js
    EWD_Assign2_WebAPI\FieldArcheryFinder

--- README_gitTemplate.md
+++
@@ -1,2 +0,0 @@
-# EWD_Assign2_WebAPI
-Integration of Web API using Node, Express &amp; MongoDB
--- start_client.js
+++ start_client.js
@@ -1,5 +1,5 @@
 const args = [ 'start' ];
 
-const opts = { stdio: 'inherit', cwd: 'C:\Users\ellingworthl\Documents\MASTERS_WIT\EWD_Assign2_SPA-WebAPI\FieldArcheryFinder', shell: true };
+const opts = { stdio: 'inherit', cwd: 'C:\Users\ellingworthl\Documents\MASTERS_WIT\EWD_Assign2_WebAPI\FieldArcheryFinder', shell: true };
 
 require('child_process').exec('npm start');
