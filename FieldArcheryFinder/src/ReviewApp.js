c6438e63bfd61f3671059ef3e40b9cd927f652e9
ellingworthl <ellingworthl@hotmail.com>
2017-05-10 14:47:09 +0100

    ReviewApp.js API correction (Assign2)
    changed api to ./reviewsApi from ./eventsApi'

--- FieldArcheryFinder/src/ReviewApp.js
+++ FieldArcheryFinder/src/ReviewApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import * as api from './eventsApi';
+import * as api from './reviewsApi';
 //import api from './data/stubAPI';
 
 //new import to support routing
